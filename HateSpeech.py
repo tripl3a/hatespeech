c40028fd1e8edbaf3d3777d1e7883b284d34b614
tripl3a <triple_a@mailbox.org>
2018-05-02 17:42:04 +0200

    small modifications

--- HateSpeech.py
+++ HateSpeech.py
@@ -10,7 +10,6 @@ from os import chdir
 chdir("/home/arndt/git-reps/nlp/01_hatespeech/")
 
 import sklearn.metrics as skm
-import math
 import fastText
 import pandas as pd
 import csv
@@ -30,34 +29,33 @@ def train_model(train_index, test_index):
     return fastText.train_supervised("data/train_data.txt")
     
 def score_model(model, test_index):
-    
     test_labels=df["label"].iloc[test_index].apply(str.replace,args=("__label__",""))
     pred_labels=df["comment_text"].iloc[test_index].apply(lambda x:model.predict(x)[0][0]).apply(str.replace,args=("__label__",""))
-    
-    #print("confusion matrix:")
-    #print(str(skm.confusion_matrix(test_labels, pred_labels)))
-    #print(str(skm.classification_report(test_labels, pred_labels)))
+    print("confusion matrix:")
+    print(str(skm.confusion_matrix(test_labels, pred_labels)))
+    print(str(skm.classification_report(test_labels, pred_labels)))
     print("f1 macro: %0.4f" % (skm.precision_recall_fscore_support(test_labels, pred_labels, average='macro')[2]))
     print("f1 micro: %0.4f" % (skm.precision_recall_fscore_support(test_labels, pred_labels, average='micro')[2]))
+    print("\n")
 
 def get_predictions(model, test_index, k=1):
     df[["label","comment_text"]].iloc[test_index].to_csv("test_data.txt",index=False,sep=" ",header=False,escapechar=" ",quoting=csv.QUOTE_NONE)
     return df["comment_text"].iloc[test_index].apply(lambda x:model.predict(x, k=k))
-    
 
 #%%
 
-# http://scikit-learn.org/stable/developers/contributing.html#rolling-your-own-estimator
-# https://github.com/vishnumani2009/sklearn-fasttext
+# Build multiple models using K-Folds
 
-from os import chdir
-chdir("/home/arndt/git-reps/nlp/01_hatespeech/")
-from skfasttext import SimpleFastTextClassifier
+import numpy as np
+from sklearn import model_selection
 
-train_file="data/train_data.txt"
-test_file="data/test_data.txt"
+seed = 77
+kfold = model_selection.KFold(n_splits=3, random_state=seed)
+#kfold = model_selection.KFold(n_splits=3, shuffle=True)
 
-clf=SimpleFastTextClassifier()
-model = clf.fit(train_file)
-predictions = clf.predict(test_file, k_best=2)
+for train_index, test_index in kfold.split(df):
+    model = train_model(train_index, test_index)
+    score_model(model, test_index)
+
+
 
